818a59fcbf18215ab5734eda08066e8e8eace7be
davidblj <david.blj95@gmail.com>
2017-09-24 21:03:17 -0500

    Algunos campos para la oferta

--- client/src/app/pages/employer-page/job-form/job-form.component.ts
+++ client/src/app/pages/employer-page/job-form/job-form.component.ts
@@ -8,6 +8,8 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class JobFormComponent implements OnInit {
 
+  model;
+
   constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit() {
