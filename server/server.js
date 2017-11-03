be57f39f0c8b6fe602a677d5ae8f40385b0e2d01
Camilo Posada <cposadaa@gmail.com>
2017-11-02 19:36:14 -0500

    Se inician las pruebas

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-let express = require('express');
+// let express = require('express');
 
-let app = express();
-require('./config/config')(app);
-require('./endpoints/routes')(app);
+// let app = express();
+// require('./config/config')(app);
+// require('./endpoints/routes')(app);
+let app = require('./app/app');
 
 app.listen(app.get('port'), () => {
     console.log(`Servidor express escuchado en el puerto ${app.get('port')}`);
